92d5cb70f5862c89743452fe6e8f7f185892c918
emilyhmoore <emily.moore@wustl.edu>
2014-03-26 10:08:53 -0500

    Fixed class script
    I had saved the new class to the wrong folder. Sorry!

---
+++ BMApack/R/bma-class.R
@@ -0,0 +1,71 @@
+#' A bma class
+#' 
+#' Object of class \code{bma} as created by the \code{fitBMA} functions
+#'
+#' 
+#' An object of the class `bma' has the following slots:
+#' \itemize{
+#' \item \code{combo.coef} A list of coefficients for each possible regression
+#' \item \code{combo.fit} A vector of R-Squared values
+#' \item \code{bmk}
+#' \item \code{exp.vals}
+#' \item \code{the coefs}
+#' \item \code{coefprobs}
+#' \item \code{x} A matrix input of covariates
+#' \item \code{y} The dependent variable input (vector) 
+#' }
+#'
+#' @author Jacob Montgomery, Dino Hadzic, Jae Hee Jung, Emily Moore
+#' @export
+setClass(Class="bma",
+         representation = representation(
+           combo.coef = "list",
+           combo.fit="numeric",
+           bmk="numeric",
+           exp.vals="numeric",
+           thecoefs="matrix",
+           coefprobs="numeric",
+           x="matrix",
+           y="numeric"
+         ),
+         prototype = prototype(
+           combo.coef=list(),
+           combo.fit=numeric(),
+           bmk=numeric(),
+           exp.vals=numeric(),
+           thecoefs=matrix(),
+           coefprobs=numeric(),
+           x = matrix(),
+           y = numeric()
+         )
+)
+
+#' @export
+setMethod("initialize", "bma",
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+#' @export
+setMethod("show", "bma",
+          function(object){
+            cat("Regression Fits:", "\n")
+            print(object@combo.fit)
+            cat("\n")
+            cat("Posterior Model Odds:", "\n")
+            print(object@bmk)
+            cat("\n")
+            cat("Posterior Expected Value of Coefficients:", "\n")
+            print(object@exp.vals)
+            cat("\n")
+            cat("Posterior Probability Coefficient is Non-zero:", "\n")
+            print(object@coefprobs)
+          }
+)
+
+
+
+
+
