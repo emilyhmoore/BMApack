2da11e8660838307db9989a8d2f1654dd77bfc4b
emilyhmoore <emily.moore@wustl.edu>
2014-03-28 11:59:24 -0500

    playing around
    Think I'll be able to start the next part.

--- modelselectorfunction2.R
+++ modelselectorfunction2.R
@@ -1,4 +1,4 @@
-char.vec<-paste("var", 1:7)
+char.vec<-paste("var", 1:15)
 input<-char.vec
 ##either.or variables are those in which one is included or the other but never both
 ##all.nothing variables are those in which a set should always be included or never included 
@@ -20,37 +20,47 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
   
   ##setting variable names
   named.set<-llply(1:length(set), function(i){names[set[[i]]]}, .parallel=parallel)
-  
-  good.models<-list()
 
-  thetests<-function(i){
-    theall.nothings<-length(unique(all.nothing %in% named.set[[i]]))==1 ##we want true here
-    theeither.ors<-((length(unique(either.or %in% named.set[[i]]))==1)==FALSE | 
-                      any(all.nothing %in% named.set[[i]]==FALSE))##good models are true
-    theoneif.others<-((one.if.other %in% named.set[[i]])[1]==TRUE & 
-                      all((one.if.other %in% named.set[[i]])[2:length(one.if.other)])==FALSE)==FALSE 
-                      ##good models come out of this mess^ as TRUE so I can use all in the next part.
-    good<-all(theall.nothings, theeither.ors, theoneif.others)
-    return(good)   
-  }
-  
+    if(length(all.nothing)+length(either.or)+length(one.if.other)==0){return(named.set)
+    }else{ 
+      thetests<-function(i){
+        if(is.null(all.nothing)==TRUE){theall.nothings<-rep(TRUE, length(named.set))} else{
+          theall.nothings<-length(unique(all.nothing %in% named.set[[i]]))==1} ##we want true here
+        
+        if(is.null(either.or)==TRUE){theeither.ors<-rep(TRUE, length(named.set))} else{
+          theeither.ors<-((length(unique(either.or %in% named.set[[i]]))==1)==FALSE | 
+                            any(all.nothing %in% named.set[[i]]==FALSE))}##good models are true
+        
+        if(is.null(one.if.other)==TRUE){theoneif.others<-rep(TRUE, length(named.set))} else{
+          theoneif.others<-((one.if.other %in% named.set[[i]])[1]==TRUE & 
+                              all((one.if.other %in% named.set[[i]])[2:length(one.if.other)])==FALSE)==FALSE} 
+        ##good models come out of this mess^ as TRUE so I can use all in the next part.
+        
+        good<-all(theall.nothings, theeither.ors, theoneif.others)
+        return(good)   
+      }
+      
     ##extracting the good models. 
     good.models<-llply(1:length(named.set), thetests ,.parallel=parallel)
 
     ##These models survived the culling.
-    the.survivors<-named.set[unlist(good.models)]
+    the.survivors<-named.set[unlist(good.models)]}
 
-    return(good.models3)
-}
+    return(the.survivors)
+} ##closing the function
 
+ ##Fast if all NULLs or all present is specified but slow if there are NULLs otherwise. 
 system.time(trial<-model.selector(char.vec, 
                                   either.or=NULL, 
-                                  all.nothing=c("var 1", "var 2"),
-                                  one.if.other=c("var 3", "var 4", "var 5")
+                                  all.nothing=NULL,
+                                  one.if.other=NULL
 ))
+head(trial)
+tail(trial)
 
 ##With this specification, it takes 31 models and trims it down to 9 models. 
 ##With ten variables, it takes 1023 models and trims it down to 319. 
 ##With eleven variables, it takes 2047 and trims it down to 639.
 ##With 15 variables, it takes 32767 and trims it down to 10239, takes 
-##about 2.8 seconds on my memory poor computer for 15. 
+##about 3 seconds on my memory poor computer for 15 if ALL have some specification. 
+##if there are NULLs, it's actually really slow, though. 
