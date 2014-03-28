38cbaa3eb50c5d499908d99b5e5ace9ed8dfef30
emilyhmoore <emily.moore@wustl.edu>
2014-03-27 23:06:30 -0500

    Trying out the function
    Seems to be working. Unfortunately, it still is calculating the whole
    set of possible models and eliminating them rather than the other
    method. But it works and does all it's supposed to do for elimination
    purposes, it seems.

--- modelselectorfunction.R
+++ modelselectorfunction.R
@@ -1,10 +1,10 @@
-char.vec<-paste("var", 1:5)
+char.vec<-paste("var", 1:15)
 
 ##either.or variables are those in which one is included or the other but never both
 ##all.nothing variables are those in which a set should always be included or never included 
 ##one.if.other are variables such as squares and interactions that must include constituent terms
 
-model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=NULL){
+model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=NULL, parallel=FALSE){
   
   if(length(all.nothing)==1){stop("If specifying All.nothing, it must have at least two variables")}
   if(length(either.or)==1){stop("If specifying Either.or, it must have at least two variables")}
@@ -19,16 +19,17 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
   set<-unlist(set, recursive=F)
   
   ##setting variable names
-  named.set<-llply(1:length(set), function(i){names[set[[i]]]})
+  named.set<-llply(1:length(set), function(i){names[set[[i]]]}, .parallel=parallel)
   
   good.models<-list()
 
   if(length(all.nothing)==0){good.models<-set} else{
   ##are the variables in there?
-  all.nothingset<-llply(1:length(named.set), function(i){all.nothing %in% named.set[[i]]})
+  all.nothingset<-llply(1:length(named.set), function(i){all.nothing %in% named.set[[i]]}, .parallel=parallel)
 
   ##if the length of the unique is one, it means either all or none of the all.nothings are included
-  theall.nothings<-llply(1:length(all.nothingset),function(i){length(unique(all.nothingset[[i]]))==1})
+  theall.nothings<-llply(1:length(all.nothingset),function(i){length(unique(all.nothingset[[i]]))==1},
+                         .parallel=parallel)
 
   ##these are th emodels we want, so which ones are they??
   pick.me<-which(theall.nothings==TRUE)
@@ -39,10 +40,12 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
   
   good.models2<-list()
   if(length(either.or)==0){good.models2<-good.models} else{
-  either.orset<-llply(1:length(good.models), function(i){either.or %in% good.models[[i]]})
+  either.orset<-llply(1:length(good.models), function(i){either.or %in% good.models[[i]]},
+                      .parallel=parallel)
 
   ##if the length of the unique is one, it means either all or none of the all.nothings are included
-  theeither.ors<-llply(1:length(either.orset),function(i){length(unique(either.orset[[i]]))==1})
+  theeither.ors<-llply(1:length(either.orset),function(i){length(unique(either.orset[[i]]))==1},
+                       .parallel=parallel)
   
   pick.me2<-which(theeither.ors==FALSE)
 
@@ -51,7 +54,8 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
   
   good.models3<-list()
   if(length(one.if.other)==0){good.models3<-good.models2} else{
-  one.if.otherset<-llply(1:length(good.models), function(i){one.if.other %in% good.models[[i]]})
+  one.if.otherset<-llply(1:length(good.models), function(i){one.if.other %in% good.models[[i]]},
+                         .parallel=parallel)
   
   ##the all function returns TRUE if ALL of the values are true. This calls the model a TRUE 
   ##if the first variable in 
@@ -64,8 +68,9 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
                       function(i){
                         one.if.otherset[[i]][1]==TRUE & 
                           all(one.if.otherset[[i]][2:length(one.if.other)])==FALSE
-                      }
-                )
+                      },
+                      .parallel=parallel
+                      )
   
   pick.me3<-which(one.in.model==FALSE)
 
@@ -76,7 +81,15 @@ model.selector<-function(input, either.or=NULL, all.nothing=NULL, one.if.other=N
   return(good.models3)
 }
 
-model.selector(char.vec, either.or=NULL, all.nothing=c("var 1", "var 2"),
-               one.if.other=c("var 3", "var 4", "var 5"))
+system.time(trial<-model.selector(char.vec, 
+               either.or=NULL, 
+               all.nothing=c("var 1", "var 2"),
+               one.if.other=c("var 3", "var 4", "var 5")
+               ))
 
+##With this specification, it takes 31 models and trims it down to 9 models. 
+##With ten variables, it takes 1023 models and trims it down to 319. 
+##With eleven variables, it takes 2047 and trims it down to 639.
+##With 15 variables, it takes 32767 and trims it down to 10239, takes 
+##about 2.8 seconds on my memory poor computer for 15. 
 
