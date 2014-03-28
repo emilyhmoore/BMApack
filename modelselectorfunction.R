0c4ec9bde2c36acd0da116719428317ef524cf37
emilyhmoore <emily.moore@wustl.edu>
2014-03-27 21:09:03 -0500

    Documentation
    documentation

--- modelselectorfunction.R
+++ modelselectorfunction.R
@@ -15,11 +15,15 @@ model.selector<-function(input, either.or, all.nothing=c("var 1," "var 2"), one.
   ##setting variable names
   named.set<-llply(1:length(set), function(i){names[set[[i]]]})
   
+  ##are the variables in there?
   all.nothingset<-llply(1:length(named.set), function(i){all.nothing %in% named.set[[i]]})
   
+  ##if the length of the unique is one, it means either all or none of the all.nothings are included
   theall.nothings<-llply(1:length(all.nothingset),function(i){length(unique(all.nothingset[[i]]))==1})
   
+  ##these are th emodels we want, so which ones are they??
   pick.me<-which(theall.nothings==TRUE)
   
+  ##This is the reduced set of models
   good.models<-set[pick.me]
 }
