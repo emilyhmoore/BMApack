d2056380b93e157616d5a9121c5ae291895de318
emilyhmoore <emily.moore@wustl.edu>
2014-04-02 09:45:28 -0500

    Working
    So I should have the always and all.nothing conditions working for any
    amount of specified variables (2+), but I will need to test this. The
    either.or will work for two variables.
    
    TO DO:
    -All of this will need to have statements allowing the user to specify
    no variables for the conditions.
    -It will need to include the one.if.other type constraints
    -It will need to generalize the either.or variable to include multiple
    variables.

--- modelselector3.R
+++ modelselector3.R
@@ -38,5 +38,38 @@ thirdlist<-c(conditionallist, truefalse.list)
 ##NOTE: I didn't end up making two separate matrices of conditional and unconditional because this actually
 ##seemed to work just fine, and I think it will be shorter in the long run.
 
+models<-expand.grid(thirdlist)
 
+##This is a new matrix that has the number of models in models but has a column for each variable.
+##It then sets the names of the matrix to match the variable names of interest.
+newmatrix<-matrix(rep(0),ncol=length(char), nrow=nrow(models))
+colnames(newmatrix)<-char
+##Making into a dataframe because expand.grid returned a data frame and I want this to be the same.
+newmatrix<-as.data.frame(newmatrix)
+
+##Setting the unconditioned variables into the appropriate part of the model
+##This simply copies the values from one to the values of the others using the names stored in "therest" object
+newmatrix[,therest]<-models[,therest]
+
+##This matches the variables marked as always with the corresponding variable in the models object
+newmatrix[,always]<-models[,"alwayscond"]
+
+##This matches the all.nothing condition with the all.nothing variable.
+##Basically it just matches the TRUE or FALSE in the model spec to all of the variables included
+##in all.nothing
+newmatrix[,all.nothing]<-models[,"all.nothingcond"]
+
+##This code basically puts var 3 as TRUE when the models object says "var 3", it returns
+##FALSE for the corresponding Var 4 and vice versa. If the previous model object said FALSe
+##indicating Neither should be included, then both will be FALSE in the full spec.
+##The idea is to use the newmatrix to index for the model building in the fitBMA function.
+##As of now, it will only work if the user specifies two either.or variables, but I think it should be
+##able to generalize fairly easily. 
+
+newmatrix[,either.or[1]]<-models[,"either.orcond"]==either.or[1]
+newmatrix[,either.or[2]]<-models[,"either.orcond"]==either.or[2]
+
+
+head(models)
+head(newmatrix)
 
