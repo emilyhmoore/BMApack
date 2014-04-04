135ef7da9197b5a50e56a5dc7c0c4a0b911f250c
jaeheejung <jaeheejung@wustl.edu>
2014-04-03 22:13:35 -0500

    Revisions and documentation
    What I did:
    
    1) Although the function worked correctly, there were parts of the
    function that were causing warning messages (like "number of items to
    replace is not a multiple of replacement length") because of
    “technically” incorrect coding. I corrected that.
    
    2) The function works when NULL is assigned to any of all.nothing,
    either.or, and always.
    
    3) I revised the function to work under parallel when specified.
    
    4) I tried many things in many ways, but could not change the for loop
    to a plyr function. I think it’s okay to leave the for loop as it’s a
    simple code.
    
    5) Originally, I could not get the function to work when all variables
    are conditioned. I got the following error: Error in do.ply(i) :  task
    2 failed - "attempt to select less than one element". However, I solved
    the problem by making the function create the “truefalse.list” only
    when the length of “therest” is not zero. Now the function works even
    when all variables are conditioned.

--- modelselector3.R
+++ modelselector3.R
@@ -3,11 +3,21 @@ char<-paste("var", 1:20)
 
 char<-c(letters[1:10])
 
-modselect<-function(x=char, parallel=FALSE, all.nothing=c("a", "b", "c"), either.or=c("d", "e"),
+modselect<-function(x=char, parallel=TRUE,core=10,all.nothing=c("a","b","c"), either.or=c("d", "e"),
                     always=c("f", "g")
                     #,one.if.other=c("h", "i", "j")
                     )
   {
+  
+  library(plyr)
+  
+   #run only if parallel is TRUE
+  if(parallel==TRUE){
+  library(foreach)
+  library(multicore)
+  library(doMC)
+  registerDoMC(cores=core) ##Will need for later for parallel stuff
+  }
   
   if(length(all.nothing)==1){stop("If specifying All.nothing, it must have at least two variables")}
   if(length(either.or)==1){stop("If specifying Either.or, it must have at least two variables")}
@@ -20,17 +30,20 @@ modselect<-function(x=char, parallel=FALSE, all.nothing=c("a", "b", "c"), either
 
   ##Which variables are the conditionals? It figures this out because we want to be able to 
   ##generically separate all of the conditional type variables from the unconditional type variables.
-  theconditionals<-which(char==conditionals)
+  theconditionals<-which(char%in%conditionals)
   therest<-char[-theconditionals]
 
-  ##This is a list that will be used for the "unconditioned" variables and put into expand grid
-  ##It generates a list that says "true false" for each unconditioned variable. 
+  ##This is an empty list that will be used for the "unconditioned" variables and put into expand grid
+truefalse.list<-list()
+
+  ##The truefalse.list will not be created if there is no unconditioned variable. If there are unconditioned variables, the code generates a list that says "true false" for each unconditioned variable. 
   ##I did it this way separately for each type and then merged it because that made more sense in my head
 ##since later we differentiate between conditioned and unconditioned variables.
-truefalse.list<-list()
+if(length(therest)!=0){
 length(truefalse.list)<-length(therest)
-truefalse.list<-llply(1:length(therest), function(i){truefalse.list[i]<-c(TRUE, FALSE)})
+truefalse.list<-llply(1:length(therest), function(i){truefalse.list[[i]]<-c(TRUE, FALSE)},.parallel=parallel)
 names(truefalse.list)<-therest
+}
 
 ##This is the always condition. It means this variables always has to be in.
 alwayscond<-TRUE
@@ -59,7 +72,6 @@ thirdlist<-c(conditionallist, truefalse.list)
 ##seemed to work just fine, and I think it will be shorter in the long run.
 
 models<-expand.grid(thirdlist)
-models
 ##This is a new matrix that has the number of models in models but has a column for each variable.
 ##It then sets the names of the matrix to match the variable names of interest.
 newmatrix<-matrix(rep(0),ncol=length(char), nrow=nrow(models))
@@ -83,7 +95,7 @@ newmatrix[,all.nothing]<-models[,"all.nothingcond"]
 ##variable's name, and it returns FALSE for the other variables. If the previous model object said FALSe
 ##indicating Neither should be included, then both will be FALSE in the full spec.
 ##The idea is to use the newmatrix to index for the model building in the fitBMA function. 
-
+	
 ##had to make this a for () loop. Couldn't get the llply to work on it for some strange reason.
 for (i in 1:length(either.or)){newmatrix[,either.or[i]]<-models[,"either.orcond"]==either.or[i]}
 
