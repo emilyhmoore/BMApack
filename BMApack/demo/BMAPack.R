b5a25e50f6d7b937ae7cbc13d7ff1316d6017da2
jaeheejung <jaeheejung@wustl.edu>
2014-04-21 17:25:36 -0500

    All functions work

--- BMApack/demo/BMAPack.R
+++ BMApack/demo/BMAPack.R
@@ -1,10 +1,22 @@
-x <- matrix(rnorm(80),ncol=8)
-colnames(x) <- paste("X",1:8,sep="")
-y <- rnorm(10)
-allNothing <- c("X1","X2")
-eitherOr <- c("X5","X6")
-always <- c("X3","X4")
-fitBMA(x=x, y=y, allNothing=allNothing, eitherOr=eitherOr, always=always)
+x <- matrix(rnorm(600),ncol=20)
+colnames(x) <- paste("X",1:20,sep="")
+y <- rnorm(30)
+allNothing <- list(c("X1","X2"),c("X3","X4"))
+eitherOr <- list(c("X5","X6"),c("X7","X8"))
+always <- c("X9","X10")
+conditionals <- list(c("X11","X12"),c("X13","X14"))
+conditionedOnTheseVariables <- list(c("X15","X16"),c("X17","X18"))
 
-plot(fitBMA(x=x, y=y, allNothing=allNothing, eitherOr=eitherOr, always=always))
+g <- 3
+parallel <- TRUE
+library(multicore)
+library(doMC)
+library(foreach)
+registerDoMC(cores=2)
+
+BMAObject <- fitBMA(x, y, g, parallel, allNothing, eitherOr, always,conditionals,conditionedOnTheseVariables)
+
+BMAObject
+
+plot(BMAObject)
 
