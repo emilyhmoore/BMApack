c5f021bae64b532225d48c62bd96b97b956bd6ad
dinohadzic <dino.hadzic@wustl.edu>
2014-04-21 11:32:31 -0500

    edit to fitBMA demo
    edit to fitBMA demo

--- BMApack/demo/fitBMA.R
+++ BMApack/demo/fitBMA.R
@@ -2,7 +2,7 @@ x <- matrix(rnorm(80),ncol=8)
 colnames(x) <- paste("X",1:8,sep="")
 y <- rnorm(10)
 allNothing <- c("X1","X2")
-eitherOr <- NULL
+eitherOr <- c("X5","X6")
 always <- c("X3","X4")
 fitBMA(x=x, y=y, allNothing=allNothing, eitherOr=eitherOr, always=always)
 
