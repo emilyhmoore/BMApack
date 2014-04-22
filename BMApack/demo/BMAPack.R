9c2963a04600307e3aaca523a55fb3abfdb98a24
dinohadzic <dino.hadzic@wustl.edu>
2014-04-21 19:07:18 -0500

    editing demo to run faster
    editing demo to run faster

--- BMApack/demo/BMAPack.R
+++ BMApack/demo/BMAPack.R
@@ -1,11 +1,11 @@
-x <- matrix(rnorm(600),ncol=20)
-colnames(x) <- paste("X",1:20,sep="")
-y <- rnorm(30)
-allNothing <- list(c("X1","X2"),c("X3","X4"))
-eitherOr <- list(c("X5","X6"),c("X7","X8"))
-always <- c("X9","X10")
-conditionals <- list(c("X11","X12"),c("X13","X14"))
-conditionedOnTheseVariables <- list(c("X15","X16"),c("X17","X18"))
+x <- matrix(rnorm(220), ncol=11)
+colnames(x) <- paste("X", 1:11,sep="")
+y <- rnorm(20)
+allNothing <- list(c("X1","X2"))
+eitherOr <- list(c("X3", "X4"))
+always <- "X5"
+conditionals <- list(c("X6", "X7"))
+conditionedOnTheseVariables <- list(c("X8","X9"))
 parallel <- FALSE
 g <- 3
 
@@ -13,5 +13,13 @@ BMAObject <- fitBMA(x, y, g, parallel, allNothing, eitherOr, always,conditionals
 
 BMAObject
 
+summary(BMAObject)
+
 plot(BMAObject)
+
+
+
+
+
+
 
