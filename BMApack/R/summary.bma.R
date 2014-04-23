1f9d8f6f1b498fbbbaddc21b09d1b13c92a7080f
dinohadzic <dino.hadzic@wustl.edu>
2014-04-23 16:46:05 -0500

    documentation complete for summary.bma
    documentation complete for summary.bma

--- BMApack/R/summary.bma.R
+++ BMApack/R/summary.bma.R
@@ -1,22 +1,26 @@
 #' summary.bma Function
 #'
-#' Summarizes bma objects as returned by fitBMA
+#' Summarizes bma objects as returned by fitBMA.
 #'
 #'
 #' @return Prints summaries of coefficients and posterior model odds and returns list 
-#'  \item{expB}{Expected values of coefficients}
-#'  \item{condSE}{Standard error of coefficients conditional on the variable being included}
-#'  \item{postProbcoefs}{Posterior probability that the coefficient is included}
-#'  \item{largerZero}{Conditional posterior probability that the coefficient is larger than zero}
+#'  \item{Conditional Mean}{Expected values of coefficients}
+#'  \item{Conditional SD}{Standard error of coefficients conditional on the variable being included}
+#'  \item{p(beta!=0|Y)}{Posterior probability that the coefficient is included}
+#'  \item{p(beta>0|Y,M)}{Conditional posterior probability that the coefficient is larger than zero}
 #' @author Jacob Montgomery, Jae Hee Jung, Emily Moore, Dino Hadzic
 #' @examples
 #' 
-#' x1<-rnorm(500)
-#' x2<-rnorm(500,3,15)
-#' dep<-(x1+2*x2)+rnorm(500,4,100)
-#' covars<-cbind(x1,x2) 
-#' trial<-fitBMA(x=covars, y=dep)
-#' summary(trial)
+#' x <- matrix(rnorm(220), ncol=11)
+#' colnames(x) <- paste("X", 1:11,sep="")
+#' y <- rnorm(20)
+#' allNothing <- list(c("X1","X2"))
+#' eitherOr <- list(c("X3", "X4"))
+#' always <- "X5"
+#' conditionals <- list(c("X6", "X7"))
+#' conditionedOnTheseVariables <- list(c("X8","X9"))
+#' BMAObject <- fitBMA(x, y, g=3, parallel=FALSE, allNothing, eitherOr, always,conditionals,conditionedOnTheseVariables)
+#' summary(BMAObject)
 #' @rdname summary.bma
 #' @aliases summary.bma,ANY-method summary.bma
 #' @export
